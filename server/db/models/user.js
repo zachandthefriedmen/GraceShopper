86232d53d735901fb692181cd5a3b9b488316210
Matthew Thor <mjthor@gmail.com>
2017-10-30 16:54:26 -0500

    chore: define user model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,12 +1,24 @@
-const crypto = require('crypto')
-const Sequelize = require('sequelize')
-const db = require('../db')
+const crypto = require('crypto');
+const Sequelize = require('sequelize');
+const db = require('../db');
 
 const User = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
+  },
+  orderHistory: {
+    type: Sequelize.ARRAY(Sequelize.INT),
+    defaultValue: [],
   },
   password: {
     type: Sequelize.STRING
@@ -16,42 +28,55 @@ const User = db.define('user', {
   },
   googleId: {
     type: Sequelize.STRING
-  }
-})
+  },
+  facebookId: {
+    type: Sequelize.STRING
+  },
+  admin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+}, {
+  getterMethods: {
+    fullName() {
+      return this.firstName + ' ' + this.lastName;
+    }
+  },
+});
 
-module.exports = User
+module.exports = User;
 
 /**
  * instanceMethods
  */
 User.prototype.correctPassword = function (candidatePwd) {
-  return User.encryptPassword(candidatePwd, this.salt) === this.password
-}
+  return User.encryptPassword(candidatePwd, this.salt) === this.password;
+};
 
 /**
  * classMethods
  */
 User.generateSalt = function () {
-  return crypto.randomBytes(16).toString('base64')
-}
+  return crypto.randomBytes(16).toString('base64');
+};
 
 User.encryptPassword = function (plainText, salt) {
   return crypto
     .createHash('RSA-SHA256')
     .update(plainText)
     .update(salt)
-    .digest('hex')
-}
+    .digest('hex');
+};
 
 /**
  * hooks
  */
 const setSaltAndPassword = user => {
   if (user.changed('password')) {
-    user.salt = User.generateSalt()
-    user.password = User.encryptPassword(user.password, user.salt)
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password, user.salt);
   }
-}
+};
 
-User.beforeCreate(setSaltAndPassword)
-User.beforeUpdate(setSaltAndPassword)
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
