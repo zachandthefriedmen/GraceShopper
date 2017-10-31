399ba164280d7acdd7b38f69ce0826ec8b81ec5e
Matthew Thor <mjthor@gmail.com>
2017-10-31 16:07:06 -0500

    chore: define category put/post/delete routes

--- server/api/category.js
+++ server/api/category.js
@@ -16,3 +16,26 @@ router.get('/:id', async (req, res, next) => {
   }
   catch (err) { next(err); }
 });
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    category.update(req.body);
+    res.sendStatus(202);
+  }
+  catch (err) { next(err); }
+});
+
+router.post('/', async (req, res, next) => {
+  try { res.json(await Category.create(req.body)); }
+  catch (err) { next(err); }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  const id = req.params.id;
+
+  try { await Category.destroy({ where: { id }}); }
+  catch (err) { next(err); }
+
+  res.sendStatus(204);
+});
