b8ddd04db56b33f9515bd6b01867a5b24f612562
Matthew Thor <mjthor@gmail.com>
2017-10-31 16:16:40 -0500

    chore: define product put/post/delete routes

--- server/api/order.js
+++ server/api/order.js
@@ -26,3 +26,26 @@ router.get('/:id', async (req, res, next) => {
   }
   catch (err) { next(err); }
 });
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    order.update(req.body);
+    res.sendStatus(202);
+  }
+  catch (err) { next(err); }
+});
+
+router.post('/', async (req, res, next) => {
+  try { res.json(await Order.create(req.body)); }
+  catch (err) { next(err); }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  const id = req.params.id;
+
+  try { await Order.destroy({ where: { id }}); }
+  catch (err) { next(err); }
+
+  res.sendStatus(204);
+});
--- server/api/product.js
+++ server/api/product.js
@@ -11,3 +11,26 @@ router.get('/:id', async (req, res, next) => {
   try { res.json(await Product.findById(req.params.id)); }
   catch (err) { next(err); }
 });
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    product.update(req.body);
+    res.sendStatus(202);
+  }
+  catch (err) { next(err); }
+});
+
+router.post('/', async (req, res, next) => {
+  try { res.json(await Product.create(req.body)); }
+  catch (err) { next(err); }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  const id = req.params.id;
+
+  try { await Product.destroy({ where: { id }}); }
+  catch (err) { next(err); }
+
+  res.sendStatus(204);
+});
