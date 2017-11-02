2adc42a6de7eba98851019b09df92b7141158d23
Benjamin Odisho <Semiroundpizza8@gmail.com>
2017-11-02 16:57:02 -0500

    feat: WIP, merging master

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ProductCell from './ProductCell';
-import { fetchProducts } from '../store';
+import { fetchProducts, fetchCategories, fetchCategory } from '../store';
 
 // const allProducts = [{ id: 1, name: 'Leash', price: 4.95, description: 'This is the coolest leash you have EVER seen! It also will never break.', image: 'https://www.placecage.com/200/300' },
 //                      { id: 2, name: 'Bone', price: 0.95, description: 'This is a bone.', image: 'https://www.placecage.com/g/200/300' },
@@ -21,19 +21,20 @@ class Products extends Component {
     super(props);
     this.state = {
       nameFilter: '',
+      categoryFilter: 0,
     };
     this.changeCategory = this.changeCategory.bind(this);
   }
 
   componentWillMount() {
     this.props.fetchProductData();
+
   }
 
   changeCategory = event => {
     // Old code for filtering out products according to the selected category
-    //   props.categoryProducts = props.allProducts.filter(product => {
-    //     return product.categoryId === event.target.value;
-    //   });
+    this.props.fetchSelectedCategory(event.target.value)
+    this.setState({ categoryFilter: event.target.value });
   };
   
   changeNameFilter = event => {
@@ -41,25 +42,27 @@ class Products extends Component {
     console.log("State", this.state);
   }
 
+  // shownProducts = this.state.categoryFilter === 0 ? this.props.category.products : this.props.product
+
   render() {
     return (
       <div className="container">
         <div id="filter-bar" className="row card">
           <h4 className="col-md-2">Category</h4>
-          <form onSubmit={this.changeCategory} className="col-md-6">
+          <form className="col-md-6">
             <input type="text" name="nameFilter" placeholder="search..." onChange={this.changeNameFilter} />
-            <select>
+            <select onChange={this.changeCategory}>
               <option value="default">Choose a category</option>
               {
                 // Old text mapping out the list of category options inside of select input field
-                // props.allCategories.map(category => {
-                //   return (
-                //     <option key={category.id} value={category.id}>{category.name}</option>
-                //   );
-                // })
+                this.props.category.map(category => {
+                  return (
+                    <option key={category.id} value={category.id}>{category.name}</option>
+                  );
+                })
               }
             </select>
-            <input type="submit" value="filter" />
+            {/* <input type="submit" value="filter" /> */}
           </form>
         </div>
 
@@ -84,11 +87,15 @@ class Products extends Component {
 /**
  * CONTAINER
  */
-const mapState = ({ product }) => ({ product });
+const mapState = ({ product, category }) => ({ product, category });
 
 const mapDispatch = dispatch => ({
   fetchProductData: () => {
     dispatch(fetchProducts());
+    dispatch(fetchCategories());
+  },
+  fetchSelectedCategory: (id) => {
+    dispatch(fetchCategory(id));
   }
 });
 
