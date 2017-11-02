dba42118451adc3e196266cf2c0ca688b03319b6
Benjamin Odisho <Semiroundpizza8@gmail.com>
2017-11-02 04:45:30 -0500

    Added basic bootstrap classes to Products and ProductCell components

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,27 +1,37 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import ProductCell from './ProductCell';
-import {fetchProducts} from '../store';
+import { fetchProducts } from '../store';
 
 
 const Products = props => {
 
-  const allProducts = [ {id: 1, name: 'Leash', price: 4.95, description: 'This is the coolest leash you have EVER seen! It also will never break.', image: 'https://www.placecage.com/200/300'}, {id: 2, name: 'Bone', price: 0.95, description: 'This is a bone.', image: 'https://www.placecage.com/g/200/300'} ];
+  const allProducts = [{ id: 1, name: 'Leash', price: 4.95, description: 'This is the coolest leash you have EVER seen! It also will never break.', image: 'https://www.placecage.com/200/300' },
+                       { id: 2, name: 'Bone', price: 0.95, description: 'This is a bone.', image: 'https://www.placecage.com/g/200/300' },
+                       { id: 3, name: 'Leash', price: 4.95, description: 'This is the coolest leash you have EVER seen! It also will never break.', image: 'https://www.placecage.com/c/200/300' },
+                       { id: 4, name: 'Bone', price: 0.95, description: 'This is a bone.', image: 'https://www.placecage.com/g/200/300' },
+                       { id: 5, name: 'Leash', price: 4.95, description: 'This is the coolest leash you have EVER seen! It also will never break.', image: 'https://www.placecage.com/200/300' },
+                       { id: 6, name: 'Bone', price: 0.95, description: 'This is a bone.', image: 'https://www.placecage.com/c/200/300' },
+                       { id: 7, name: 'Leash', price: 4.95, description: 'This is the coolest leash you have EVER seen! It also will never break.', image: 'https://www.placecage.com/200/300' },
+                       { id: 8, name: 'Bone', price: 0.95, description: 'This is a bone.', image: 'https://www.placecage.com/g/200/300' },
+                       { id: 9, name: 'Bone', price: 0.95, description: 'This is a bone.', image: 'https://www.placecage.com/c/200/300' },];
 
   const changeCategory = event => {
-  //   props.categoryProducts = props.allProducts.filter(product => {
-  //     return product.categoryId === event.target.value;
-  //   });
+    // Old code for filtering out products according to the selected category
+    //   props.categoryProducts = props.allProducts.filter(product => {
+    //     return product.categoryId === event.target.value;
+    //   });
   };
 
   return (
-    <div>
-      <div id="filter-bar">
-        <h4>Category</h4>
-        <form onSubmit={changeCategory}>
-        <select>
-          <option value="default">Choose a category</option>
+    <div className="container">
+      <div id="filter-bar" className="row card">
+        <h4 className="col-md-2">Category</h4>
+        <form onSubmit={changeCategory} className="col-md-6">
+          <select>
+            <option value="default">Choose a category</option>
             {
+              // Old text mapping out the list of category options inside of select input field
               // props.allCategories.map(category => {
               //   return (
               //     <option key={category.id} value={category.id}>{category.name}</option>
@@ -33,7 +43,7 @@ const Products = props => {
         </form>
       </div>
 
-      <div>
+      <div className="row">
         {
           allProducts.map(product => {
             return (
