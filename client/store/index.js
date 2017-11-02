b35dc40d675eade740c86d4ac177e8d6af47872b
Richard Polidoro <armyofbear136@gmail.com>
2017-11-02 15:56:29 -0500

    fix: update reducers index to combine reducers from all modules

--- client/store/index.js
+++ client/store/index.js
@@ -4,9 +4,12 @@ import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import user from './user';
 import product from './product';
+import category from './category';
+import review from './review';
+import order from './order';
 
 
-const reducer = combineReducers({ user, product });
+const reducer = combineReducers({ user, product, category, review, order });
 
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
