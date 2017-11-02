280726f75e5af00e6486eb3db9b86d0c1f108700
Benjamin Odisho <Semiroundpizza8@gmail.com>
2017-11-02 04:22:26 -0500

    Fix minor indent issues

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -12,16 +12,16 @@ export const LandingPage = (props) => {
   return (
     <div className="container">
       <div className="row">
-      <div id="leftWell" className="jumbotron col-md-5">
-        <h1 className="display-3">Welcome to the Bazaar.</h1>
-        <p className="lead">Bento says, 'make yourself at home.'</p>
-        <hr className="my-2" />
-        <p>Placeholder text.</p>
-      </div>
+        <div id="leftWell" className="jumbotron col-md-5">
+          <h1 className="display-3">Welcome to the Bazaar.</h1>
+          <p className="lead">Bento says, 'make yourself at home.'</p>
+          <hr className="my-2" />
+          <p>Placeholder text.</p>
+        </div>
 
-      <div id="rightImage" className="col-md-7">
-        <img src="http://www.placecage.com/c/600/400" />
-      </div>
+        <div id="rightImage" className="col-md-7">
+          <img src="http://www.placecage.com/c/600/400" />
+        </div>
       </div>
     </div>
   );
@@ -32,7 +32,7 @@ export const LandingPage = (props) => {
  */
 const mapState = (state) => {
   return {
-  // Leftover code from user-home.js (component this was based off of) in case someone else needs it later
+    // Leftover code from user-home.js (component this was based off of) in case someone else needs it later
     // email: state.user.email
   };
 };
