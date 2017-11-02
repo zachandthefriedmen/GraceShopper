4c63c9cfbc1e08e22b97fcc0b72a38f31528f7b2
Benjamin Odisho <Semiroundpizza8@gmail.com>
2017-11-02 05:11:38 -0500

    Fixed what might be conflicts

--- client/components/ProductCell.js
+++ client/components/ProductCell.js
@@ -4,13 +4,11 @@ import { Link } from 'react-router-dom';
 const ProductCell = ({ product }) => {
 
   return (
-    <div className="col-md-4">
-      <Link to={`/products/${product.id}`}>
-        <img src={product.image} />
-        <h2>{product.name}</h2>
-        <h3>{product.price}</h3>
-      </Link>
-    </div>
+    <Link to={`/products/${product.id}`}>
+      <img src={product.image} />
+      <h2>{product.name}</h2>
+      <h3>{product.price}</h3>
+    </Link>
   );
 };
 
