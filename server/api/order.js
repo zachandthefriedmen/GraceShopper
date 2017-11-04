e3cfa4f10803e1542d4020f0b2af1cf273bde1f4
Matthew Thor <mjthor@gmail.com>
2017-11-04 12:02:19 -0700

    fix: update order api to eager load order-products

--- server/api/order.js
+++ server/api/order.js
@@ -17,27 +17,30 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-    let orderInfo = await Order.findById(req.params.id, {
+    let order = await Order.findById(req.params.id, {
       include: [{
         model: User,
         attributes: ['id', 'email', 'firstName', 'lastName', 'admin']
       }]
     });
+
+    let orderProducts = await order.getProducts();
+
     // THE PROBLEM HERE IS ASYNC ISSUES. NOT MODIFYING orderProducts before sending response!!
-    let orderProducts = await OrderProduct.findAll({ where: { orderId: req.params.id }});
-    orderProducts.forEach(async product => {
-      let productInfo = await Product.findById(product.dataValues.productId);
-      product.dataValues.productId = productInfo;
-    });
+    // let orderProducts = await OrderProduct.findAll({ where: { orderId: req.params.id }});
+    // orderProducts.forEach(async product => {
+    //   let productInfo = await Product.findById(product.dataValues.productId);
+    //   product.dataValues.productId = productInfo;
+    // });
     //MAP version... working maybe?? testing above with forEach first.
     // orderProducts.map(async product => {
     //   let newProduct = product;
     //   let productInfo = await Product.findById(product.productId);
     //   newProduct.productId = productInfo;
     //   return newProduct;
-    // });
-    console.log('ORDER PRODUCTS BEFORE RESPONSE: ', orderProducts);
-    res.json({orderInfo, orderProducts});
+    // });O
+    // console.log('ORDER PRODUCTS BEFORE RESPONSE: ', orderProducts);
+    res.json({order, orderProducts});
   }
   catch (err) { next(err); }
 });
