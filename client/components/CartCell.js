e0da569fe58cd36040ede695b5ded958cb0b5f3b
Benjamin Odisho <Semiroundpizza8@gmail.com>
2017-11-05 13:07:46 -0600

    Added basic bootstrap CSS to cart

--- client/components/CartCell.js
+++ client/components/CartCell.js
@@ -4,16 +4,35 @@ import { Link } from 'react-router-dom';
 const CartCell = ({ product }) => {
 
   return (
-    <div id="cart-item">
-      {/* Link around each items name, leading to corresponding SingleProduct page */}
-      <Link to={`/products/${product.id}`}>
-        <h4>{product.name}</h4>
-      </Link>
-        <h5>Quantity:##</h5>
-        <h5>Unit Cost:{product.price}</h5>
-        <h5>Quantity Cost:##</h5>
-        <img src="http://www.placecage.com/100/100" />
-      <button id="delete">delete</button>
+
+    // <div id="cart-item" className="card col-md-3">
+    //   {/* Link around each items name, leading to corresponding SingleProduct page */}
+    //   <img src="http://www.placecage.com/800/400" className="card-img-top img-fluid" />
+    //   <div id="cart-content" className="card-body">
+    //     <h4 className="card-title"> </h4>
+    //     <p className="card-text">
+    //       Quantity:##
+    //     Unit Cost:{product.price}
+    //       Quantity Cost:##
+    //     </p>
+    //     <button id="delete">delete</button>
+    //   </div>
+    // </div>
+
+    <div className="col-md-4">
+      <div className="card">
+        <img className="card-img-top img-fluid" src={product.image} />
+        <div className="card-block">
+          <h4 className="card-title"><Link to={`/products/${product.id}`}>
+            {product.name}
+          </Link></h4>
+          <p className="card-text">
+            Quantity:## <br />
+            Unit Cost:{product.price} <br />
+            Quantity Cost:##
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
