237c68f57079dd876890f383e75e64d4ab4a0717
Benjamin Odisho <Semiroundpizza8@gmail.com>
2017-11-05 13:13:20 -0600

    Removed unessicary comments

--- client/components/CartCell.js
+++ client/components/CartCell.js
@@ -4,21 +4,6 @@ import { Link } from 'react-router-dom';
 const CartCell = ({ product }) => {
 
   return (
-
-    // <div id="cart-item" className="card col-md-3">
-    //   {/* Link around each items name, leading to corresponding SingleProduct page */}
-    //   <img src="http://www.placecage.com/800/400" className="card-img-top img-fluid" />
-    //   <div id="cart-content" className="card-body">
-    //     <h4 className="card-title"> </h4>
-    //     <p className="card-text">
-    //       Quantity:##
-    //     Unit Cost:{product.price}
-    //       Quantity Cost:##
-    //     </p>
-    //     <button id="delete">delete</button>
-    //   </div>
-    // </div>
-
     <div className="col-md-4">
       <div className="card">
         <img className="card-img-top img-fluid" src={product.image} />
