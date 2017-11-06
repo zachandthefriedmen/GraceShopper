e9651c9adf4c2e4aae420d7f6f54b3da678cb37b
Richard Polidoro <armyofbear136@gmail.com>
2017-11-06 11:37:35 -0600

    fix: move orders by user id to users routes and move reviews by product id to products route

--- server/api/order.js
+++ server/api/order.js
@@ -23,28 +23,6 @@ router.get('/:id', async (req, res, next) => {
         attributes: ['id', 'email', 'firstName', 'lastName', 'admin']
       }]
     });
-<<<<<<< HEAD
-    
-    // THE PROBLEM HERE IS ASYNC ISSUES. NOT MODIFYING orderProducts before sending response!!
-
-
-
-    //essential find for orderProdcuts for both Promise.all approach and async await approach
-    let orderProducts = await OrderProduct.findAll({ where: { orderId: req.params.id } });
-
-    //WIP TESTING PROMISE.ALL APPROACH
-    //let productsArray = Promise.all(orderProducts.map(product => Product.findById(product.dataValues.productId)));
-
-
-    //current logic, that works but is not blocking
-    orderProducts.forEach(async product => {
-      let productInfo = await Product.findById(product.dataValues.productId);
-      product.dataValues.productId = productInfo;
-    });
-
-
-
-=======
 
     let orderProducts = await order.getProducts();
 
@@ -54,24 +32,15 @@ router.get('/:id', async (req, res, next) => {
     //   let productInfo = await Product.findById(product.dataValues.productId);
     //   product.dataValues.productId = productInfo;
     // });
->>>>>>> e3cfa4f10803e1542d4020f0b2af1cf273bde1f4
     //MAP version... working maybe?? testing above with forEach first.
     // orderProducts.map(async product => {
     //   let newProduct = product;
     //   let productInfo = await Product.findById(product.productId);
     //   newProduct.productId = productInfo;
     //   return newProduct;
-<<<<<<< HEAD
-    // });
-
-
-    console.log('ORDER PRODUCTS BEFORE RESPONSE: ', orderProducts);
-    res.json({ orderInfo, productsArray });
-=======
     // });O
     // console.log('ORDER PRODUCTS BEFORE RESPONSE: ', orderProducts);
     res.json({order, orderProducts});
->>>>>>> e3cfa4f10803e1542d4020f0b2af1cf273bde1f4
   }
   catch (err) { next(err); }
 });
@@ -85,14 +54,6 @@ router.put('/:id', async (req, res, next) => {
   catch (err) { next(err); }
 });
 
-router.get('/user/:id', async (req, res, next) => {
-  try {
-    res.json(await Order.findAll({
-      where: { userId: req.params.id }
-    }));
-  }
-  catch (err) { next(err); }
-});
 
 router.post('/', async (req, res, next) => {
   try {
