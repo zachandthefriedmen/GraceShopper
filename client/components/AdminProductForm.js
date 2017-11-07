3c8de601b3bc50af67820f743100abaf9d1db9d4
Benjamin Odisho <Semiroundpizza8@gmail.com>
2017-11-06 23:44:33 -0600

    chore: add Create Product form for admin products form and introduced basic bootstrap

--- client/components/AdminProductForm.js
+++ client/components/AdminProductForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { editProduct } from '../store';
+import { editProduct, createProduct } from '../store';
 import { Link } from 'react-router-dom';
 
 
@@ -9,8 +9,21 @@ const AdminProductForm = (props) => {
   const { products } = props;
   console.log(props);
 
+  // Creates a new product when form is submitted
+  let submitCreateProduct = (event) => {
+    event.preventDefault();
+    let newProduct = {
+      name: event.target.productName.value,
+      price: +event.target.productPrice.value,
+      image: event.target.productImage.value,
+      description: event.target.productBody.value,
+      available: true,
+      rating: 5.0,
+    };
+    props.createNewProduct(newProduct);
+  };
 
-  // Edits product corresponding to editProductForm.select, changing fields with new values.
+  // Edits product corresponding to editProductForm.select when form is submitted, changing fields with new values.
   let submitEditProduct = (event) => {
     event.preventDefault();
     let productChanges = {
@@ -23,48 +36,74 @@ const AdminProductForm = (props) => {
     props.editProductInfo(event.target.productSelect.value, productChanges);
   };
 
+  let disableProductClick = (id) => {
+    // Logic for disabling a product, wanted to wait and see if everyone wanted a button for this or would rather do it in the edit form before implimenting
+    // props.editProductInfo(id, );
+  };
+
+
+  //TODO: Add categories for products
+  console.log(products);
   if (!products) return (<div />);
 
   return (
-    <div>
-      <table id="productsTable">
-        <tbody>
-          <tr>
-            <th>ID</th>
-            <th>AVAILABLE</th>
-            <th>NAME</th>
-            <th>PRICE</th>
-            <th>RATING</th>
-            <th>OPTIONS</th>
-          </tr>
-          {
-            products.map(product => {
-              return (
-                <tr key={product.id}>
-                  <td>{product.id}</td>
-                  <td>{product.available}</td>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>{product.rating}</td>
-                  <td>OPTIONS</td>
-                </tr>
-              );
-            })
-          }
-        </tbody>
-      </table>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-9">
+          <table className="table table-striped" id="productsTable">
+            <thead className="thead-dark">
+              <tr>
+                <th>ID</th>
+                <th>AVAILABLE</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>RATING</th>
+                <th>OPTIONS</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                products.map(product => {
+                  return (
+                    <tr key={product.id}>
+                      <td>{product.id}</td>
+                      <td>{product.available}</td>
+                      <td>{product.name}</td>
+                      <td>{product.price}</td>
+                      <td>{product.rating}</td>
+                      <td><button onClick={() => disableProductClick(product.id)}><i className="fa fa-times" /></button></td>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+        <div className="col-md-3">
 
-      <h4>Edit Products Form</h4>
-      <form id="editProductForm" onSubmit={submitEditProduct}>
-        <label>Select a product to edit:</label>
-        <select name="productSelect" form="editProductForm">
-          {/* Creates an option in the select for each individual product */}
-          {products.map(product => (<option key={product.key} value={product.id}>{product.name}</option>))}
-        </select>
-        <input name="productName" type="text" placeholder="New Name" />
-        <input name="productPrice" type="number" step=".01" placeholder="New Price" />
-        <input name="submitNewProduct" type="submit" />
-      </form>
+        <h4>Create Products Form</h4>
+          <form id="createProductForm" className="form-group" onSubmit={submitCreateProduct}>
+            <label>Select a product to edit:</label>
+            <input name="productName" className="form-control" type="text" placeholder="Name" />
+            <input name="productBody" className="form-control" type="text" placeholder="Body" />
+            <input name="productImage" className="form-control" type="text" placeholder="Image URL" />
+            <input name="productPrice" className="form-control" type="number" step=".01" placeholder="Price" />
+            <input name="submitNewProduct" type="submit" />
+          </form>
+
+          <h4>Edit Products Form</h4>
+          <form id="editProductForm" className="form-group" onSubmit={submitEditProduct}>
+            <label>Select a product to edit:</label>
+            <select name="productSelect" form="editProductForm">
+              {/* Creates an option in the select for each individual product */}
+              {products.map(product => (<option key={product.key} value={product.id}>{product.name}</option>))}
+            </select>
+            <input name="productName" className="form-control" type="text" placeholder="New Name" />
+            <input name="productPrice" className="form-control" type="number" step=".01" placeholder="New Price" />
+            <input name="submitEditProduct" type="submit" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
@@ -77,6 +116,9 @@ const mapState = state => {
 
 const mapDispatch = (dispatch) => {
   return {
+    createNewProduct: (product) => {
+      dispatch(createProduct(product));
+    },
     editProductInfo: (id, product) => {
       dispatch(editProduct(id, product));
     }
