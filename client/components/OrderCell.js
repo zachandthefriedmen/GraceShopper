0f4fcb26be8b098a62efbb7fec458cb5b62037d4
Richard Polidoro <armyofbear136@gmail.com>
2017-11-07 16:10:45 -0600

    fix: format order history, intelligently display integrations

--- client/components/OrderCell.js
+++ client/components/OrderCell.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const OrderCell = ({ order }) => {
-  console.log('order', order);
+  let totalQuantity = 0;
+  let totalPrice = 0;
+  order.orderProducts.forEach(op => {
+    totalQuantity += op.quantity;
+    totalPrice += (op.quantity * op.price);
+  });
+
   return (
+
     <div className="col-md-3">
-      <h2>Order Number: {order.id}</h2>
-      <h3>Order Total: {//order.totalPrice
-      }</h3>
-      <h3>Order Date: {order.orderDate}</h3>
+      <h3>Order Number: <Link to={`/order/${order.id}`}>{order.id}</Link></h3>
+      <h4>Order Total: ${totalPrice.toFixed(2)}</h4>
+      <h4>Order Quantity: {totalQuantity}</h4>
+      <h4>Order Date: {order.orderDate}</h4>
     </div>
   );
 };
